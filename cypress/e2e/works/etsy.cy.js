c899127c372cbb0a61984f2499d83e5c7c4cfbcb
Fatih I <117350581+fatihh34@users.noreply.github.com>
2023-09-15 18:22:09 +0300

    15.09

--- cypress/e2e/works/etsy.cy.js
+++ cypress/e2e/works/etsy.cy.js
@@ -4,7 +4,7 @@ describe("", function() {
     
     
         
-        beforeEach(() => {
+        before(() => {
             
            cy.visit("https://www.etsy.com") 
            //cy.url().should("eq","https://www.etsy.com")
@@ -13,17 +13,45 @@ describe("", function() {
             
         });
 
+        it('down bottom for locating an element', () => {
+            cy.scrollTo("bottom");
+            cy.get(':nth-child(3) > .wt-hide-xs').should("contain","About")
+            cy.scrollTo("top")
+
+            cy.get('#global-enhancements-search-query').type("phone{enter}")
+            cy.get('#sortby > .wt-menu__trigger').click()
+
+
+
+        });
+
+
         it('search something and add to cart', () => {
 
+            cy.wait(2000)
+
+            const siralamaOptions = ['Relevancy', 'Lowest Price', 'Highest Price', 
+            'Top Customer Reviews', 'Most Recent'];
+
+            siralamaOptions.forEach((secenek) => {
+                cy.get('#sortby > .wt-menu__body a').
+                contains(secenek).
+                should('be.visible')
+                .then(() => {
+                    cy.log('"${secenek}" secenek gorulur');
+                });
+            });
 
 
+//cy.get('#sortby > .wt-menu__body')
 
 
-    });
+        });
+
+    })
     
     
     
     
     
     
-})
